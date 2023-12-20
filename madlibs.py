ffbdc6e86424616983f4655b1c6905979b83d324
Tuba Kahraman <tubakaraman1375@gmail.com>
2023-12-20 10:13:10 +0300

    Madlibs için commit

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+noun = input("Noun: ")
+noun2 = input("Noun: ")
+noun3 = input("Noun: ")
+noun4 = input("Noun: ")
+verb = input("Verb: ")
+verb2  = input("Verb: ")
+
+madlibs = "Hey! welcome to my {noun}. here, we {verb} about {noun2} and we have with {noun3}. see you {noun4}."
+
+print(madlibs)
